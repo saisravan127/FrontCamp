3b16dc9cf468b8242efd85aa5b795b9ec00c6385
saisravan127 <saisravan_ab@epam.com>
2022-06-20 10:32:47 +0530

    removed extra files

--- Assignment 2.1/printarray.js
+++
@@ -1,9 +0,0 @@
-//Q1
-// http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-
-
-function printArray(array){
-    return array.join(",");
-  }
-
-console.log(printArray(["h","e","l","l","o"]));
