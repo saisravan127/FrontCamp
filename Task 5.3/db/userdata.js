86d1122a1ddd505085cf30acd0955d3214841b04
saisravan127 <saisravan_ab@epam.com>
2022-06-20 10:34:42 +0530

    add Task 5.3 files

---
+++ Task 5.3/db/userdata.js
@@ -0,0 +1,12 @@
+export let users=[
+    {
+        "id":"1",
+        "login":"sravan",
+        "password":"abcdef",
+        "age":21,
+        "isDeleted":false
+    }
+    
+]
+
+console.log(users);
---
+++ Task 5.3/src/controller/getUser.js
@@ -0,0 +1,13 @@
+import { users } from "../../db/userdata.js";
+
+export const getUser = (req, res)=>{
+    const {id}=req.params;
+    const user= users.find(u=> u.id===users.id);
+    if(!user || user.isDeleted){
+        res.status(404).json({message: "User not found!"});
+        return;
+    }
+    res.status(200).json(user);
+}
+
+//export default {getUser};
