78c2f52ef29e73c05d71681642f9c0b0aa1c8db5
saisravan127 <saisravan_ab@epam.com>
2022-07-04 08:08:25 +0530

    Added angular Task 2 and 3

--- angular1/src/app/manual-component/manual-component.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-    selector: 'app-manual-component',
-    templateUrl: './manual-component.component.html',
-    styleUrls: ['./manual-component.component.css']
-})
-export class ManualComponentComponent {
-}
